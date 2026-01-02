1abb3fd3037b3f9d27196016043288c06dbb59a5
junbyeol <yoonjs0510@gmail.com>
2026-01-02 21:54:11 +0900

    feat(web); add calendar component

--- be-steady-web/src/App.tsx
+++ be-steady-web/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { VerticalCalendar } from './component/vertical-calendar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-blue-500 w-full h-full">
+      {/* header 뷰 */}
+      <div className="flex bg-red-500 relative h-24">
+        <h1 className="text-4xl font-bold self-start">준성타마 프로젝트</h1>
+        <img src={viteLogo} className="absolute bottom-0 right-1/2 w-32 h-32 z" alt="React logo" />        
+        <div className="flex flex-row flex-1 justify-center gap-4 self-end">
+          <StatisticCard />
+          <StatisticCard />
+        </div>
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          {import.meta.env.VITE_SERVER_HOST}
-        </p>
+      {/* task 뷰 */}
+      <div className="flex-1 flex justify-center items-center bg-green-500">
+        <div className="w-full max-w-lg h-full max-h-[800px]">
+          <VerticalCalendar />
+        </div>
       </div>
-    </>
+    </div>
+  )
+}
+
+function StatisticCard() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-white h-16 w-32 rounded-lg border-gray-300 border-2">
+      <h6>전체 운동횟수</h6>
+      <p>100<span>일</span></p>
+    </div>
   )
 }
 
