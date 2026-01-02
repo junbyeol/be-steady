349e7ec9ccdd19f48f8817edd0715f9a037dfdd7
junbyeol <yoonjs0510@gmail.com>
2026-01-02 20:54:30 +0900

    chore(web): install tailwindcss

--- be-steady-web/src/App.tsx
+++ be-steady-web/src/App.tsx
@@ -24,10 +24,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          {import.meta.env.VITE_SERVER_HOST}
+        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
